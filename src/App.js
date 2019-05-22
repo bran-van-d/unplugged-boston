a926918635473193ceafd90256676b8e4364bd6b
Brandon Wieber <brandon.m.wieber@gmail.com>
2019-05-21 20:43:58 -0400

    css update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex-column">
+      <div className="logo-header">
+        <img src="unplugged-icon.jpg" width="200" height="200" alt="gaming-icon" />
+      </div>
+
+      <div className="body-section flex-row">
+        <div className="about-us">
+          <span className="center-text"> ABOUT US </span>
+        </div>
+
+        <div className="event-wrapper">
+          <div className="towns-and-shops">
+            <span className="center-text"> TOWNS AND SHOPS </span>
+          </div>
+
+          <div className="event-connection">
+            <span className="center-text"> EVENT CONNECTION </span>
+          </div>
+        </div>
+
+        <div className="calendar">
+          <span className="center-text"> CALENDAR </span>
+        </div>
+      </div>
     </div>
   );
 }
