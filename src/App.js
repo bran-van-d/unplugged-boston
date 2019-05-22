5d30f26cf4fe8899326162a0e3febb9cb11889dc
Brandon Van Dyck <bvandyck@shyftanalytics.com>
2019-05-21 22:05:53 -0400

    Get mock calendar setup on site

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import events from './events';
+import dates from './dates';
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+
+
+const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
+
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
 
 function App() {
   return (
     <div className="App flex-column">
       <div className="logo-header">
-        <img src="unplugged-icon.jpg" width="200" height="200" alt="gaming-icon" />
+        <div className=" logo-text center-text"> Unplugged Gaming Boston </div>
       </div>
 
       <div className="body-section flex-row">
         <div className="about-us">
-          <span className="center-text"> ABOUT US </span>
+          <p> We're a crew of gamers in and around the Boston area! Whether you are from Boston, Somerville, Beverly, Quincy, etc, we have a game for you ! </p>
+        
+          <p> We plan, coordinate and chat on our Discord Channel. If you have questions why not simply join us? Click below to join the Discord! </p>
+          <a href="https://discord.gg/yYUPtQm"> 
+            <img src="discord-icon.svg" alt="gaming-icon" />
+          </a>
         </div>
 
         <div className="event-wrapper">
-          <div className="towns-and-shops">
-            <span className="center-text"> TOWNS AND SHOPS </span>
+          <div className="towns-and-shops flex-column">
+            <h2> Primary Gaming Locations </h2>
+            <div className="shop-container flex-column">
+              <a target="_blank" rel='noreferrer noopener' href="http://www.pandemoniumbooks.com/"> Pandemonium -- Cambridge </a>
+              <a target="_blank" rel='noreferrer noopener' href="http://comicazi.com/site/"> Comicazi -- Somerville </a>
+              <a target="_blank" rel='noreferrer noopener' href="https://twitter.com/knightmovescafe"> Knight's Moves Cafe -- Somerville/Brookline</a>
+            </div>
           </div>
 
           <div className="event-connection">
@@ -24,7 +43,15 @@ function App() {
         </div>
 
         <div className="calendar">
-          <span className="center-text"> CALENDAR </span>
+          <BigCalendar
+            events={events}
+            views={allViews}
+            step={60}
+            showMultiDayTimes
+            max={dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours')}
+            defaultDate={new Date(2015, 3, 1)}
+            localizer={localizer}
+          />
         </div>
       </div>
     </div>
